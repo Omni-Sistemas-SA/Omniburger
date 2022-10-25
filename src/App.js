77732cb7f959cb340bda2d1246bb50f51cc27d1b
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-24 21:39:36 -0500

    Se añadieron los enlaces de la pagina de inicio y la pagina del perfil

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/navegacion/Navbar.js'
-
-
+import Inicio from './components/pages/Inicio.js'
+import Perfil from './components/pages/Perfil.js'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar></Navbar>
+        <Routes>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/perfil' element={<Perfil />} />
+        </Routes>
       </Router>
     </div>
   );
