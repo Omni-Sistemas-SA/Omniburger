65514f10ac34169814aaa5f003cb1d3204634d06
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-26 20:34:58 -0500

    Se añadió el footer a la app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/navegacion/Navbar.js'
 import Inicio from './components/pages/Inicio.js'
 import Perfil from './components/pages/Perfil.js'
+import Footer from './components/footer/Footer.js'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' element={<Inicio />} />
             <Route path='/perfil' element={<Perfil />} />
         </Routes>
+        <Footer></Footer>
       </Router>
     </div>
   );
