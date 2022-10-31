5ada9d7349989a27be1aafec1eaad42b6a06cd63
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-10-30 19:41:28 -0500

    Se añadieron los modulos de autenticación

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react' //importar librerias de auth0
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-shw3azpq.us.auth0.com'
+      clientId='ZDGIgZTIbDguIt6ZYq6vMcUQys6kj4i2'
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
