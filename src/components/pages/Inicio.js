f0a769063d6c4c6c520a0d6efa03178ab818f96e
Yully Moreno <yullyalejandra.morenorodriguez@gmail.com>
2022-11-01 18:58:37 -0500

    Se actualizaron los componentes de pages

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -2,11 +2,17 @@ import React from "react";
 import pagesCss from './pages.css';
 import Carrusel from "../carrusel/Carrusel";
 import Card from "../card/Card";
-import datosProductos from '../../data/productos.json'
+import datosProductosJSON from '../../data/productos.json'
 import CarritoOffcanvas from "../carrito/CarritoOffcanvas";
 
 const Inicio = () => {
-
+    let datosProductos 
+    if(localStorage.getItem("productos")==null){
+        localStorage.setItem("productos", JSON.stringify(datosProductosJSON))
+        datosProductos = JSON.parse(localStorage.getItem("productos"));
+    } else {
+         datosProductos = JSON.parse(localStorage.getItem("productos"));
+    }
     return (
         <div className="pages">
             <Carrusel />
--- src/components/pages/Perfil.js
+++ src/components/pages/Perfil.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import CarritoOffcanvas from '../carrito/CarritoOffcanvas';
-// import DatosUsuario from '../datos-usuario/DatosUsuario';
+import Admin from '../admin/Admin';
+import AddProducto from '../addProducto/AddProducto';
+import AdminVentas from '../admin/AdminVentas';
 
 const Perfil = () => {
 
-    return (
-        <div>
-          <h2 className="pages__titulo">Perfil de Usuario</h2>
-          {/* <DatosUsuario/> */}
-          <CarritoOffcanvas/>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="pages__titulo">Perfil de Usuario</h2>
+      <div className='container border border-secondary rounded-5'>
+        <h3 className='p-5'>Administrar Productos</h3>
+        <Admin></Admin>
+        <AddProducto />
+      </div>
+      <div className='container border border-secondary rounded-5 mt-5 mb-5'>
+        <h3 className='p-5'>Administrar Ventas</h3>
+        <AdminVentas />
+      </div>
+      <CarritoOffcanvas />
+    </div>
+  )
 };
 export default Perfil;
