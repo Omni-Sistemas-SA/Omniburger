631a1085db3719bb249f20c98dd0e621ddc40a32
monicamaria <monicamaria1218@gmail.com>
2022-11-01 17:35:04 -0500

    se agregó la actualización de los componentes del carrito

--- src/components/carrito/Carrito.js
+++ src/components/carrito/Carrito.js
@@ -4,13 +4,16 @@ import carritoCSS from "./carrito.css"
 
 const Carrito = () => {
 
+    const listp = JSON.parse(localStorage.getItem("lista"));
+    
     return (
         <div>
             <Link className="position-relative carrito__icono" to="/" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                 <img className='navegacion__icono ' src=".\img\iconos\Carrito.svg" />
                 <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill carrito__icono__contador">
-                    {
-                        localStorage.getItem("numProd")
+                    {listp.length !== null
+                        ? listp.length
+                        : 0
                     }
                     <span class="visually-hidden">cantidad de productos</span>
                 </span>
